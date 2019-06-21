988e17fe96b5e3fad6ce7749b933951d4f446814
gcompiano <gucompi@gmail.com>
2019-06-21 10:16:40 -0300

    Finalizado, metodo 'Buscador' y all

--- app.js
+++ app.js
@@ -3,10 +3,40 @@ var app = express();
 var fs = require('fs');
 app.get('/*', function (req, res) {
     console.log(req.path)
-    fs.readFile('./api'+req.path+'.json',(err,data)=>{
-        if(err)  res.send(err);
-        res.send(JSON.parse(data))
-    })
+    var fileContents;
+    try {
+     
+      if(req.path && req.path.indexOf('search')!=-1){
+        console.log("search")
+        busqueda = req.path.split('search/')
+        respuesta=[]
+        allFile=fs.readFileSync('./api/all.json')
+        allJson=JSON.parse(allFile)
+
+        allJson.forEach(superHeroe => {
+          
+          if(superHeroe.name.toLowerCase().indexOf(busqueda[1].toLowerCase())!=-1){
+            console.log("superHeroe")
+            console.log(superHeroe.name)
+            console.log("busqueda")
+            console.log(busqueda)
+
+            respuesta.push(superHeroe)
+          }
+        });
+        res.status(200).json(respuesta)
+      }
+      else{
+
+        fileContents = fs.readFileSync('./api'+req.path+'.json');
+        res.status(200).json(JSON.parse(fileContents))
+      }
+    } catch (err) {
+      // Here you get the error when the file was not found,
+      // but you also get any other error
+      res.status(500).json({err:'Problema'})
+      console.log(err)
+    }
 });
 
 app.listen(3000, function () {
